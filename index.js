37dfa32cc47b856d309c7ffe8310301a6bbc1a12
matteo <matteo@MacBook-Air-de-matteo.local>
2025-06-25 11:17:55 +0200

    bouton suivant et rejouer

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const questionnaire = [
        {answer:"A.Ghana"},
         {answer:"B. Afrique du Sud"},
         {answer:"C. Nigeria"},
-        {answer:"D. Côte d’Ivoire"}],
+        {answer:"D. Côte d’Ivoire"},
+        {reponseLudique: "L’Afrobeat est né au Nigeria dans les années 1970 grâce à Fela Kuti, qui a fusionné les rythmes traditionnels africains, le jazz et le funk avec des messages politiques forts."}],
     },
 
 {
@@ -15,16 +16,18 @@ const questionnaire = [
        {answer:"A. Yoruba"},
         {answer:"B. Massaï"},
         {answer:"C. Touareg"},
-        {answer:"D.Pygmée"}],
+        {answer:"D.Pygmée"},
+        {reponseLudique: "Fela Kuti, lui-même d’origine yoruba, a puisé dans les rythmes, chants et spiritualités de ce peuple pour bâtir la base rythmique et mélodique de l’Afrobeat."}],
     },
 
 {
       text: "Quel artiste est connu pour avoir popularisé le mélange rap et afrobeat avec des sons comme Afro Trap Part.3 ?",
       options: [
-       {answer:"A.Niska<"},
+       {answer:"A.Niska"},
         {answer:"B.  MHD"},
         {answer:"C.Burna Boy"},
-        {answer:"D.Olamide"}],
+        {answer:"D.Olamide"},
+        {reponseLudique:" MHD a lancé le style 'Afro Trap', un genre hybride qui a conquis la jeunesse francophone et remis à l’honneur les influences africaines dans le rap."}],
     },
 {
       text: "Quel instrument est le plus typique dans les instrus afrobeat ?",
@@ -32,7 +35,8 @@ const questionnaire = [
        {answer:"A.Guitare électrique"},
         {answer:"B.Balafon"},
         {answer:"C.Percussions / batterie africaine"},
-        {answer:"D.Harmonica"}],
+        {answer:"D.Harmonica"},
+        {reponseLudique:"Les percussions sont au cœur de l’Afrobeat : elles créent un rythme hypnotique et puissant qui pousse à danser tout en transmettant un message."}],
     },
 {
       text: "Quel artiste a été l’un des premiers à parler de la banlieue dans le rap français dans les années 90 ?",
@@ -40,14 +44,48 @@ const questionnaire = [
        {answer:"A.IAM"},
         {answer:"B. Kery James"},
         {answer:"C.MC Solaar"},
-        {answer:"D. NTM"}],
+        {answer:"D. NTM"},
+        {reponseLudique:"NTM (JoeyStarr et Kool Shen) a été pionnier dans l’expression de la colère des jeunes des quartiers populaires."}],
     },
     
 ]
 
+let currentQuestionIndex = 0 
+let buttonNext = document.getElementById("next-question")
+function loadQuestion(){
+     document.querySelector('h2').innerHTML = questionnaire[currentQuestionIndex].text
+  document.getElementById('reponses1').innerText = questionnaire[currentQuestionIndex].options[0].answer
+  document.getElementById('reponses2').innerText = questionnaire[currentQuestionIndex].options[1].answer
+  document.getElementById('reponses3').innerText = questionnaire[currentQuestionIndex].options[2].answer
+  document.getElementById('reponses4').innerText = questionnaire[currentQuestionIndex].options[3].answer
 
+}
+function nextButton(){
+    currentQuestionIndex += 1
+   
+    loadQuestion()
+   
+}
 
+function replayButton(){ 
+  currentQuestionIndex=0
+  loadQuestion()
+  /* document.querySelector('h2').innerHTML = questionnaire[currentQuestionIndex].text
+  document.getElementById('reponses1').innerText = questionnaire[currentQuestionIndex].options[0].answer
+  document.getElementById('reponses2').innerText = questionnaire[currentQuestionIndex].options[1].answer
+  document.getElementById('reponses3').innerText = questionnaire[currentQuestionIndex].options[2].answer
+  document.getElementById('reponses4').innerText = questionnaire[currentQuestionIndex].options[3].answer */
+   //document.getElementById("replay-button").style.display = "none"
+  document.getElementById("next-question").style.display="inline-block"
+}
 
+if(currentQuestionIndex === 4 )
+{
+     document.getElementById("replay-button").style.display = "none"
+}
+else{
+     document.getElementById("replay-button").style.display = "block"
+}
 
 
 
