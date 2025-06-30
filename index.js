9906d23a4b973cbf6c07acbb941b1e745d1c07c6
Ursula Eponon <ulaeponon@gmail.com>
2025-06-30 16:36:27 +0200

    Save my local changes

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const questionnaire = [
       { answer: "C.MC Solaar" },
       { answer: "D. NTM" }],
     correct: 3,//Reponse D
-    reponseLudique:"NTM (JoeyStarr et Kool Shen) a été pionnier dans l’expression de la colère des jeunes des quartiers populaires.",
+    reponseLudique: "NTM (JoeyStarr et Kool Shen) a été pionnier dans l’expression de la colère des jeunes des quartiers populaires.",
   },
 
 ]
@@ -61,17 +61,41 @@ function showPopup(titreMessage, reponseLudique) {
   const message = `${titreMessage}\n\n${reponseLudique}`;
   document.getElementById("pop-upMessage").innerText = message;
   document.getElementById("pop-up").style.display = "flex";
-  document.getElementById("popup-nextbtn").style.display="block";
+  document.getElementById("popup-nextbtn").style.display = "block";
 }
 
-function closePopup(){
+function closePopup() {
   document.getElementById("pop-up").style.display = "none";
-  document.getElementById("popup-nextbtn").disabled= false;
+  document.getElementById("popup-nextbtn").disabled = false;
+  document.getElementById("popup-nextbtn").style.display = "none";
+
+}
+let timer = 10;
+let timeInterval;
+function startTimer(duration) {
+  let timeLeft = duration;
+  const timeDisplay = document.getElementById("quiz-timer");
+  timeDisplay.innerText = `Temps restant: ${timeLeft}S`
+  timeInterval = setInterval(() => {
+    timeLeft--;
+    timeDisplay.innerText = `Temps restant: ${timeLeft}S`;
+    console.log(timeLeft)
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      timeDisplay.innerText = "Temps écoulé !";
+      document.querySelectorAll("#quiz-options button").forEach(btn => btn.disabled = true);
+      showPopup("Temps écoulé !", "La bonne réponse était : " + questionnaire[currentIndex].options[questionnaire[currentIndex].correct].answer);
+    } 
+    
+  }
+    , 1000);
 }
 
 
 let currentIndex = 0;
 let score = 0;
+
+
 function showQuestion(index) {
   const currentQuestion = questionnaire[index];
   const questionDiv = document.getElementById("quiz-questions");
@@ -83,21 +107,21 @@ function showQuestion(index) {
     btn.innerText = option.answer;
     btn.onclick = () => {
       const buttons = document.querySelectorAll("#quiz-options button");
-    buttons.forEach(btn => btn.disabled = true);
+      buttons.forEach(btn => btn.disabled = true);
+       clearInterval(timeInterval);
       if (i === currentQuestion.correct) {
-      showPopup("Bonne réponse !",currentQuestion.reponseLudique);
-      score++;
+        showPopup("Bonne réponse !", currentQuestion.reponseLudique);
+        score++;
       } else {
-         showPopup("Mauvaise réponse !",currentQuestion.reponseLudique);
-     
-      
-         
+        showPopup("Mauvaise réponse !", currentQuestion.reponseLudique);
       }
     };
     optionsDiv.appendChild(btn);
-    });
+    clearInterval(timeInterval);
+    startTimer(10);
+  });
 }
-    
+
 
 function nextQuestion() {
   currentIndex++;
@@ -108,7 +132,7 @@ function nextQuestion() {
     document.getElementById("quiz-questions").innerText = `Quiz terminé ! Ton score est : ${score}/${questionnaire.length}`;
     document.getElementById("quiz-options").innerHTML = "";
     document.getElementById("popup-nextbtn").style.display = "none";
-    document.getElementById("next-question").style.display= "inline-block";
+    document.getElementById("next-question").style.display = "inline-block";
   }
 }
 showQuestion(currentIndex)
@@ -119,10 +143,11 @@ document.getElementById("popup-nextbtn").onclick = function () {
 };
 
 
-function replayButton(){
-  currentIndex=0
-  score=0
+function replayButton() {
+  currentIndex = 0
+  score = 0
   showQuestion(currentIndex)
-  document.getElementById("popup-nextbtn").style.display="inline-block";
-  document.getElementById("next-question").style.display="none";
+  document.getElementById("popup-nextbtn").style.display = "inline-block";
+  document.getElementById("next-question").style.display = "none";
+  clearInterval(timeInterval);
 }
